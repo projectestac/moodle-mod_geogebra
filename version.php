b46af671809faa27bfc72c39dca14d68fc10ff09
Sara Arjona <sarjona@xtec.cat>
2011-11-11 14:05:32 +0100

    Reviewed version.php file to add Moodle version

--- version.php
+++ version.php
@@ -1,4 +1,7 @@
 <?php
-$module->version  = 2011091200;  // If version == 0 then module will not be installed
+$module->version  = 2011111100;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 0;           // Period for cron to check this module (secs)
+$module->requires = 2007101512;  // Requires this Moodle version
+
+$release = "1.9.1 (20111009)";     // User-friendly version number
 ?>
