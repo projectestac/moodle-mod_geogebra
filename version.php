bd2511dba4081b269aa86c6f58344521c0c1dd53
Sara Arjona <sarjona@xtec.cat>
2012-06-05 18:17:51 +0200

    Reviewed version.php file to add maturity parameter

--- version.php
+++ version.php
@@ -1,6 +1,7 @@
 <?php
-$module->version  = 2012030101;  // The current module version (Date: YYYYMMDDXX)
-$module->cron     = 0;           // Period for cron to check this module (secs)
-$module->requires = 2007101512;  // Requires this Moodle version
-$module->release  = 'v1.0.0 Beta';
-?>
+$module->version   = 2012030101;      // The current module version (Date: YYYYMMDDXX)
+$module->requires  = 2007101512;      // Requires this Moodle version
+$module->cron      = 0;               // Period for cron to check this module (secs)
+$module->component = 'mod_jclic';     // To check on upgrade, that module sits in correct place
+$module->release   = 'v0.9.0';        // Human-readable version name
+$module->maturity  = MATURITY_RC;     // How stable the plugin is
