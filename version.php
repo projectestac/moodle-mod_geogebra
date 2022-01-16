7bd9aacff5740ea6cff208644c7b0b6e15d02c45
Francesc Busquets <francesc@gmail.com>
2022-01-16 19:33:37 +0100

    Update changelog and version.php

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021120700;      // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2022011600;      // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015111600;      // Requires this Moodle version (2.7)
 $plugin->cron      = 0;               // Period for cron to check this module (secs)
 $plugin->component = 'mod_geogebra';  // To check on upgrade, that module sits in correct place
-$plugin->release   = 'v3.6.5';        // Human-readable version name
+$plugin->release   = 'v3.6.6';        // Human-readable version name
 $plugin->maturity  = MATURITY_STABLE; // How stable the plugin is
