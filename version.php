a7903b05a77ff562c7a1737f6f3a17f9ce319d19
Francesc Busquets <francesc@gmail.com>
2022-06-09 13:21:31 +0200

    Add random value to tmpdir name

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022060700;      // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2022060900;      // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015111600;      // Requires this Moodle version (2.7)
 $plugin->cron      = 0;               // Period for cron to check this module (secs)
 $plugin->component = 'mod_geogebra';  // To check on upgrade, that module sits in correct place
-$plugin->release   = 'v3.6.7';        // Human-readable version name
+$plugin->release   = 'v3.6.8';        // Human-readable version name
 $plugin->maturity  = MATURITY_STABLE; // How stable the plugin is
